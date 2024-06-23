b4b3916daba7b3cc59aea403e0e7e3ac25faf5b6
DESKTOP-01ILGOI\conta <contato@software-solution-it.com>
2024-06-22 21:13:54 -0300

    job

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿// Controllers/JobController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace track_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobController : ControllerBase
+    {
+        private readonly JobService _jobService;
+
+        public JobController(JobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        [HttpPost("startJob")]
+        public IActionResult StartJob()
+        {
+            _jobService.StartJob();
+            return Ok("Job started.");
+        }
+    }
+}
